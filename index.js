a325992077dd4cda0367da08b72a57b06ebb2496
Bruno Paula <devbrunopaula@gmail.com>
2021-06-03 22:59:30 -0400

    initial commit

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server')
 const port = process.env.PORT || 5000
 
 server.listen(port, () => {
-  console.log('Server Running')
+  console.log(`Server Running on ${port}`)
 })
